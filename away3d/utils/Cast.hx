3f2b50d6dfde50551bf628444ea47f4b663f3093
Jay Sistar <jaysistar@yahoo.com>
2015-03-26 15:26:47 -0400

    For Haxe 3.2 fixes away3d/away3d-core-openfl#45

--- away3d/utils/Cast.hx
+++ away3d/utils/Cast.hx
@@ -47,8 +47,7 @@ class Cast {
     }
 
     static public function tryColor(data:Dynamic):Int {
-        if (Std.is(data, UInt)) return cast(data, UInt);
-        if (Std.is(data, Int)) return cast(data, UInt);
+        if (Std.is(data, Int)) return data;
         if (Std.is(data, String)) {
             if (data == "random") return Std.int(Math.random() * 0x1000000);
             if (_colorNames == null) {
@@ -196,9 +195,11 @@ class Cast {
                 _colorNames.set("transparent", 0xFF000000);
             }
 
+            var dataString:String = data;
             if (_colorNames.exists(data))
                 return _colorNames.get(data);
-            if ((( cast(data, String) ).length == 6) && isHex(data)) return Std.parseInt("0x" + data);
+            if ((dataString.length == 6) && isHex(data))
+                return Std.parseInt("0x" + dataString);
         }
         return 0xFFFFFF;
     }
