4ccc5226209e85bc4fa2a8b9fb5230594b5dc7fe
P.J.Shand <pete.shand@imagination.com>
2015-05-09 19:18:20 +1000

    add contextIndex to view3D constructor

--- away3d/core/managers/Stage3DProxy.hx
+++ away3d/core/managers/Stage3DProxy.hx
@@ -543,7 +543,7 @@ class Stage3DProxy extends EventDispatcher {
         // ugly stuff for backward compatibility
         var renderMode:Context3DRenderMode = (forceSoftware) ? Context3DRenderMode.SOFTWARE : Context3DRenderMode.AUTO;
         #if flash
-            _stage3D.requestContext3D(renderMode);
+            _stage3D.requestContext3D(cast renderMode);
         #else
             _stage3D.requestContext3D(Std.string(renderMode));
         #end
